f512862ad32b3628ae2101aee942972bf3b39b79
mutwakilmo <mutwakilmo@gmail.com>
2020-02-12 18:28:42 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/mutwakilmo/android/simpleclactest/CalculatorTest.java
@@ -0,0 +1,32 @@
+package com.mutwakilmo.android.simpleclactest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Mutwakil Mo on
+ */
+public class CalculatorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addT() {
+    }
+
+    @Test
+    public void sub() {
+    }
+
+    @Test
+    public void div() {
+    }
+
+    @Test
+    public void mul() {
+    }
+}
---
+++ app/src/main/java/com/mutwakilmo/android/simpleclactest/Calculator.java
@@ -0,0 +1,38 @@
+package com.mutwakilmo.android.simpleclactest;
+
+/**
+ * Created by Mutwakil Mo on 1212
+ */
+public class Calculator {
+
+    // Available operations
+    public enum Operator {ADD, SUB, DIV, MUL}
+
+    /**
+     * Addition operation
+     */
+    public double add(double firstOperand, double secondOperand) {
+        return firstOperand + secondOperand;
+    }
+
+    /**
+     * Subtract operation
+     */
+    public double sub(double firstOperand, double secondOperand) {
+        return firstOperand - secondOperand;
+    }
+
+    /**
+     * Divide operation
+     */
+    public double div(double firstOperand, double secondOperand) {
+        return firstOperand / secondOperand;
+    }
+
+    /**
+     * Multiply operation
+     */
+    public double mul(double firstOperand, double secondOperand) {
+        return firstOperand * secondOperand;
+    }
+}
